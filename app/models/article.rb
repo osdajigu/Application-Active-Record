ce5a7cb2642f0ad4de501eb8769e18c42e2ee80d
Alan Navarro <alanjnavarrom@gmail.com>
2016-08-29 11:18:59 -0500

    Comments functionality added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
 	validates :tittle, presence: true, uniqueness: true;
 	validates :body, presence: true, length: {minimum: 20};
+	has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828183913) do
+ActiveRecord::Schema.define(version: 20160829152423) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "tittle"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20160828183913) do
     t.integer  "visits_count"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
