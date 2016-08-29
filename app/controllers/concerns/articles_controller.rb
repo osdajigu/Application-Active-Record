357dc042da6c72e1759fd02445869de4d8c74a6a
Alan Navarro <alanjnavarrom@gmail.com>
2016-08-29 10:12:32 -0500

    Complete functionality

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(tittle: params[:article][:tittle], body: params[:article][:body])
+		@article = Article.new(article_params)
 
 		if @article.save
 			redirect_to @article	
@@ -28,4 +28,21 @@ class ArticlesController < ApplicationController
 		redirect_to articles_path
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:tittle, :body)
+	end
 end
