949cbb5460fc8f029fd1dffa49efcaa68155b9bf
Alan Navarro <alanjnavarrom@gmail.com>
2016-08-30 18:59:48 -0500

    Links a agregar y menu principal agregados

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
  
   #only, or except
-  root 'welcome#index'
+  root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
